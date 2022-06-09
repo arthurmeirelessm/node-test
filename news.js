9490c6c19c4ad9c288df1fe1a38b317c88b227ad
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-09 20:09:59 -0300

    Create enviroment express for development

--- news.js
+++ news.js
@@ -1,16 +1,16 @@
 var http = require('http')
 
 
-http.createServer((req , res) => {
+http.createServer((req, res) => {
 	var category = req.url
 	if (category == '/tech') {
-       res.end('<h1>ROUTE OF TECH</h1>')
+		res.end('<h1>ROUTE OF TECH</h1>')
 	} else if (category == '/mobile') {
-        res.end('<h1>ROUTE OF MOBILE</h1>')
-	}else if (category == '/clothing') {
-        res.end('<h1>ROUTE OF CLOTHING</h1>')
-	}else if (category == '/shoe') {
-        res.end('<h1>ROUTE OF SHOE</h1>')
+		res.end('<h1>ROUTE OF MOBILE</h1>')
+	} else if (category == '/clothing') {
+		res.end('<h1>ROUTE OF CLOTHING</h1>')
+	} else if (category == '/shoe') {
+		res.end('<h1>ROUTE OF SHOE</h1>')
 	}
-  res.end('<html><body>News portal<body><html>')
+	res.end('<html><body>News portal<body><html>')
 }).listen(3000);
